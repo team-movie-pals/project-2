00be33e71cc1fd1afa2cae98cc8d2697ed54c8d3
SilviaZlo <silvianevesmayer@gmail.com>
2023-04-02 03:13:26 -0400

    Commit changes to index.html file - I added the genre id numbers to the form, as well as the genre Romance, which I had previously forgotten.

--- script.js
+++ script.js
@@ -10,3 +10,5 @@
 // go over each item/genre of VAR3 --> if item also exists inside VAR 1, get the content of "original_title" from inside VAR 1 (upcoming movies API) and store it in a variable VAR4 (=var4 will contain the movie titles according to genres selection)
 
 // console.log VAR4 on the section
+
+//=======================================
